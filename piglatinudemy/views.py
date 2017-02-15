4394aa8aeb546db40afa4e6220d727d1f94f7a80
AntLukeLiu <antmliu@gmail.com>
2017-02-15 14:16:09 -0500

    functional pig latin translator

--- piglatinudemy/views.py
+++ piglatinudemy/views.py
@@ -1,4 +1,23 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
-def hello(request):
-    return HttpResponse("Hello World!")
+def home(request):
+    return render(request, 'home.html')
+
+def translate(request):
+    original = request.GET['originaltext']
+
+    translation = ''
+
+    for word in original.lower().split():
+        if word[0] in['a','e', 'i', 'o', 'u']:
+            #know it is a vowel
+            translation += word
+            translation += 'yay '
+        else:
+            #consonant
+            translation += word[1:]
+            translation += word[0]
+            translation += 'ay '
+
+    return HttpResponse(translation )
